6c255fe763f17e8a8dcf66163f7920c18dbf5d09
David Pal <david_pal@epam.com>
2023-05-10 13:35:41 +0200

    Add ES Module imports to task2

--- module-1/task2.ts
+++ module-1/task2.ts
@@ -8,10 +8,25 @@
 // line). - THIS MUST BE A TYPO, streams should not load everything into memory anyway, so I will use streams...line by line ie: chunk by chunk?
 // • In case of read/write errors, log them in the console.
 // • The program should be started via npm script using nodemon (i.e. npm run task2).
+
+/*
+COMMONJS
+
 const path = require('path');
 const csv = require('csvtojson');
 const { createReadStream, createWriteStream } = require('fs');
 const { pipeline } = require('node:stream/promises');
+
+*/
+
+/*
+ES MODULES
+*/
+
+import path from 'path';
+import csv from 'csvtojson';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'node:stream/promises';
 
 async function writeCSVToText() {
   await pipeline(
@@ -22,5 +37,5 @@ async function writeCSVToText() {
 }
 
 writeCSVToText()
-  .then(() => 'Pipeline succeeded')
+  .then(() => console.log('Pipeline succeeded'))
   .catch((e: { code: string }) => console.log(e.code));
