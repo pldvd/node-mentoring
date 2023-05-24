8eddab440dc3ac17906507fae6490de55c4685df
David Pal <david_pal@epam.com>
2023-05-24 17:06:36 +0200

    Create user data

---
+++ module-2/data/users.ts
@@ -0,0 +1,40 @@
+export type User = {
+  id: string;
+  login: string;
+  password: string;
+  age: number;
+  isDeleted?: boolean;
+};
+
+export const users: User[] = [
+  {
+    id: '1',
+    login: 'login1',
+    password: 'secret1',
+    age: 18,
+  },
+  {
+    id: '2',
+    login: 'login2',
+    password: 'secret2',
+    age: 28,
+  },
+  {
+    id: '3',
+    login: 'login3',
+    password: 'secret3',
+    age: 38,
+  },
+  {
+    id: '4',
+    login: 'login4',
+    password: 'secret4',
+    age: 48,
+  },
+  {
+    id: '5',
+    login: 'login5',
+    password: 'secret5',
+    age: 58,
+  },
+];
