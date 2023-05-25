c57dd6db9eef8be5c42deb928c7e0fccdec5889e
David Pal <david_pal@epam.com>
2023-05-25 17:53:43 +0200

    Create Joi schema

---
+++ module-2/schema/index.ts
@@ -0,0 +1,16 @@
+import Joi from 'joi';
+
+export const userSchema = Joi.object({
+  id: Joi.string(),
+  login: Joi.string().required(),
+  password: Joi.string()
+    .alphanum()
+    .pattern(new RegExp(/^(?=.*[a-zA-Z])(?=.*[0-9])/)),
+  age: Joi.number().min(4).max(130),
+  isDeleted: Joi.bool(),
+});
+
+export const filterIngSchema = Joi.object({
+  loginSubstring: Joi.string(),
+  limit: Joi.number().optional,
+});
