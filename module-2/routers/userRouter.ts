9a7571a22e36da28431e58a4216f694c75c0115e
David Pal <david_pal@epam.com>
2023-05-31 14:25:09 +0200

    Remove express req and res objects from service functions

--- module-2/routers/userRouter.ts
+++ module-2/routers/userRouter.ts
@@ -1,18 +1,48 @@
 import express from 'express';
-import * as userService from '../services';
+import { StatusCodes } from 'http-status-codes';
+import UserService from '../services';
 import { findUser } from '../middleware/findUser';
 import { validateUserData, validateFilters } from '../middleware/validators';
 
 const userRouter = express.Router();
+const userService = new UserService();
 
-userRouter.get('/', validateFilters, userService.getUsers);
+userRouter.get('/', validateFilters, async (req, res) => {
+  const users = await userService.getUsers();
 
-userRouter.get('/:id', findUser, userService.returnUser);
+  res.status(StatusCodes.OK).json(users);
+});
 
-userRouter.put('/:id', validateUserData, findUser, userService.updateUser);
+userRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const user = await userService.getUser(id);
 
-userRouter.delete('/:id', findUser, userService.deleteUser);
+  if (!user) {
+    res.status(StatusCodes.NOT_FOUND).send('User not found.');
+  } else {
+    res.status(StatusCodes.OK).json(user);
+  }
+});
 
-userRouter.post('/', validateUserData, userService.createUser);
+userRouter.put('/:id', validateUserData, findUser, async (req, res) => {
+  const { id } = req.params;
+  const updatedUser = userService.updateUser(id);
+
+  res.status(StatusCodes.OK).json(updatedUser);
+});
+
+userRouter.delete('/:id', findUser, async (req, res) => {
+  const { id } = req.params;
+  const deletedUser = await userService.deleteUser(id);
+
+  res.status(StatusCodes.OK).json(deletedUser);
+});
+
+userRouter.post('/', validateUserData, async (req, res) => {
+  const userData = req.body;
+  const newUser = await userService.createUser(userData);
+
+  res.status(StatusCodes.CREATED).json(newUser);
+});
 
 export default userRouter;
