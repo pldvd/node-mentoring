84b3154b38866a9b4fed7e5188a20019c81ed735
David Pal <david_pal@epam.com>
2023-06-05 14:06:36 +0200

    Create script for creating and populating groups table

--- module-2/data-access/db-init.sql
+++ module-2/data-access/db-init.sql
@@ -12,3 +12,19 @@ VALUES
     ('user1', 'password1', 10),
     ('user2', 'password2', 20),
     ('user3', 'password3', 30);
+
+DROP TYPE IF EXISTS permissions_type;
+CREATE TYPE permissions_type AS ENUM ('READ', 'WRITE', 'DELETE', 'SHARE', 'UPLOAD_FILES');
+
+CREATE TABLE IF NOT EXISTS groups
+(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    permissions permissions_type ARRAY NOT NULL
+);
+
+INSERT INTO groups (name, permissions)
+VALUES
+    ('users', '{READ, SHARE}'),
+    ('admins', '{READ, WRITE, SHARE, UPLOAD_FILES}'),
+    ('superadmins', '{READ, WRITE, DELETE, SHARE, UPLOAD_FILES}');
