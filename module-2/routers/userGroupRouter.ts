dec00230884cbad7b418dfbcbb4edfc5dd916564
David Pal <david_pal@epam.com>
2023-06-06 18:02:15 +0200

    Create userGroupService.getAll()

--- module-2/routers/userGroupRouter.ts
+++ module-2/routers/userGroupRouter.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import UserGroup from '../models/UserGroup';
+import UserGroupService from '../services/userGroupService';
 
 const userGroupRouter = express.Router();
+const userGroupService = new UserGroupService(UserGroup);
 
-userGroupRouter.get('/', (req, res) => {
-  res.send('Hello world!');
+userGroupRouter.get('/', (req, res, next) => {
+  userGroupService
+    .getAll()
+    .then((userGroups) => {
+      res.status(200).json(userGroups);
+    })
+    .catch(next);
 });
 
 export default userGroupRouter;
