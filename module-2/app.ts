a453cabf78a2e0026f9ad3e6c1d53bb6c655f4e6
David Pal <david.pal@lseg.com>
2023-07-18 16:23:30 +0200

    Add cors to the project

--- module-2/app.ts
+++ module-2/app.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
 import appRouter from './routers';
 import { errorHandler, notFound } from './middleware/errorHandlers';
 import { requestLogger } from './middleware/requestLogger';
@@ -10,6 +11,7 @@ import logger from './utils/logger';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 app.use(appRouter);
