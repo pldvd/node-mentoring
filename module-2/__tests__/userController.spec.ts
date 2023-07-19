cb341aa0fa83b3cd25ac4c058846b1b79dae4fc8
David Pal <david.pal@lseg.com>
2023-07-19 15:23:01 +0200

    Define basic test scenarios

--- module-2/__tests__/userController.spec.ts
+++ module-2/__tests__/userController.spec.ts
@@ -5,21 +5,29 @@ import sequelizeFixtures from 'sequelize-fixtures';
 import appRouter from '../routers';
 import { users } from './fixtures/users';
 import app from '../app';
+import sequelize from '../data-access';
 
-const populateDB = async () => {
-  await sequelizeFixtures.loadFixture(users, { User });
+const populateDB = () => {
+  return sequelizeFixtures
+    .loadFixtures(users, { User })
+    .then(() => console.log('all good'));
 };
 
-const clearDB = async () => {
-  await User.destroy({ truncate: true });
-};
+// const clearDB = () => {
+//   return User.destroy({ truncate: true });
+// };
 
 describe('userController', () => {
-  // beforeEach(populateDB);
-  // afterEach(clearDB);
+  // beforeEach(() => populateDB());
+  // afterEach(() => clearDB());
+  // afterAll(() => sequelize.close());
 
-  it('Should return all users upon a request to GET /users', async () => {
-    const users = await request(app).get('/users');
-    expect(1).toBe(1);
-  });
+  // it('Should return all users upon a request to GET /users', (done) => {
+  //   request(app).get('/users').expect(401, done);
+  // });
+
+  test('GET /users should return a list of users', () => {});
+  test('GET /users/:id should return a specific user', () => {});
+  test('POST /users should create a new user', () => {});
+  test('DELETE /users should return return a specific user', () => {});
 });
